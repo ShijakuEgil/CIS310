b1adaafcbb391369bd6b3080d4fe40313642a31b
egish <egish@DESKTOP-S4HOJJJ>
2017-01-10 23:54:13 -0500

    program 0 1st comit

--- Number Conversion Program/Number Conversion Program/NrConversionPrgSource.cpp
+++ Number Conversion Program/Number Conversion Program/NrConversionPrgSource.cpp
@@ -8,40 +8,47 @@ Prof. David Yoon
 #include<stack>
 #include<iomanip>
 #include<stack>
+#include<string>
 using namespace std;
 
-int convertToBinary(int i)
+string convertToBinary(int i)
 {
 	
+	int b = i;
 	int r;
-	int q = i;
+	int q;
+	string nr;
 	stack<int> binNr;
-	while (q > 0)
+	if (b == 0)
+		return 0;
+	while (b > 0)
 	{
-		r = q % 2;
-		binNr.push(r);
-		q = q / 2;
+		r = b % 2;
+		nr.insert(0, 1, r);
+		q = b / 2;
+		b = q;
 	}
-	
+	return nr;
 }
+
 int convertToHex(int i)
 {
-
+	return 0;
 }
 int convertToBCD(int i)
 {
-
+	return 0;
 }
 
 int main()
 {
-	cout << "Decimal" << setw(5) << "Binary" << setw(5) 
-		<< "Hexadecimal" << setw(5) << "BCD" << endl;
+	cout << "Decimal" << setw(15) << "Binary" << setw(15) 
+		<< "Hexadecimal" << setw(15) << "BCD" << endl;
 
-	for (int i = 0; i <= 255; i++)
+	for (int i = 0; i < 256; i++)
 	{
-		cout << i << setw(5) << convertToBinary(i) << setw(5) 
-			<< convertToHex(i) << setw(5) << convertToBCD(i) << endl;
+		cout << i << setw(15) << convertToBinary(i) << setw(15) 
+			<< convertToHex(i) << setw(15) << convertToBCD(i) << endl;
 	}
 	return 0;
 }
