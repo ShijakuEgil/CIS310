852f74e427d1479c2acb672502a6554d4c6da5bf
egish <egish@DESKTOP-S4HOJJJ>
2017-01-11 21:32:53 -0500

    program zero 1/11

--- Number Conversion Program/Number Conversion Program/NrConversionPrgSource.cpp
+++ Number Conversion Program/Number Conversion Program/NrConversionPrgSource.cpp
@@ -11,29 +11,43 @@ Prof. David Yoon
 #include<string>
 using namespace std;
 
+string hexAlph[]={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+
 string convertToBinary(int i)
 {
-	
 	int b = i;
 	int r;
 	int q;
 	string nr;
-	stack<int> binNr;
+
 	if (b == 0)
-		return 0;
+		return "0";
 	while (b > 0)
 	{
 		r = b % 2;
-		nr.insert(0, 1, r);
+		nr.insert(0, to_string(r));
 		q = b / 2;
 		b = q;
 	}
 	return nr;
 }
 
-int convertToHex(int i)
+string convertToHex(int i)
 {
-	return 0;
+	int b = i;
+	int r;
+	int q;
+	string nr;
+	if (b == 0)
+		return "0";
+	while (b > 0)
+	{
+		r = b % 16;
+		nr.insert(0,hexAlph[r]);
+		q = b / 16;
+		b = q;
+	}
+	return nr;
 }
 int convertToBCD(int i)
 {
@@ -47,7 +61,7 @@ int main()
 
 	for (int i = 0; i < 256; i++)
 	{
-		cout << i << setw(15) << convertToBinary(i) << setw(15) 
+		cout << i << setw(15) << convertToBinary(i)<< setw(15) 
 			<< convertToHex(i) << setw(15) << convertToBCD(i) << endl;
 	}
 	return 0;
